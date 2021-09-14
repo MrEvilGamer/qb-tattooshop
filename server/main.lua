3a2f48a67e4b056959eaaa3355dcbaa08abd0992
Aditya Nath <84228628+MrEvilGamer@users.noreply.github.com>
2021-09-14 10:37:31 +0530

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,117 +1,73 @@
 QBCore.Functions.CreateCallback('SmallTattoos:GetPlayerTattoos', function(source, cb)
-    local src = source
+	local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    
-    if Player then
-        exports['oxmysql']:execute('SELECT tattoos FROM players WHERE citizenid = @citizenid', {
-            ['@citizenid'] = Player.PlayerData.citizenid
-        }, function(result)
-            if result[1].tattoos then
-                cb(json.decode(result[1].tattoos))
-            else
-                cb()
-            end
-        end)
-    else
-        cb()
-    end
+
+	if Player then
+		exports.oxmysql:fetch('SELECT tattoos FROM players WHERE citizenid = ?', { Player.PlayerData.citizenid }, function(result)
+			if result[1].tattoos then
+				cb(json.decode(result[1].tattoos))
+			else
+				cb()
+			end
+		end)
+	else
+		cb()
+	end
 end)
 
 QBCore.Functions.CreateCallback('SmallTattoos:PurchaseTattoo', function(source, cb, tattooList, price, tattoo, tattooName)
-    local src = source
+	local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    
-    if Player.PlayerData.money.cash >= price then
-        Player.Functions.RemoveMoney('cash', price)
-        TriggerClientEvent('QBCore:Notify', source, "You bought a tattoo", "success")
-    else
-        TriggerClientEvent('QBCore:Notify', source, "not enough cash", "error")
-    end
-    table.insert(tattooList, tattoo)
-    
-    exports['oxmysql']:execute('UPDATE players SET tattoos = @tattoos WHERE citizenid = @citizenid', {
-        ['@tattoos'] = json.encode(tattooList),
-        ['@citizenid'] = Player.PlayerData.citizenid
-    })
-    
-    print("You have bought the ~y~" .. tattooName .. "~s~ tattoo for ~g~$" .. price)
-    cb(true)
-    
-    TriggerClientEvent('Apply:Tattoo', source, tattoo, tattooList)
+	local PlayerMoney = {
+        cash = Player.PlayerData.money.cash,
+        bank = Player.PlayerData.money.bank,
+    }
+
+	if PlayerMoney.bank >= price then
+		Player.Functions.RemoveMoney("bank", price , "purchases-tattoos")
+		table.insert(tattooList, tattoo)
+
+		exports.oxmysql:execute('UPDATE players SET tattoos = ? WHERE citizenid = ?', {json.encode(tattooList), Player.PlayerData.citizenid})
+
+		cb(true)
+
+		TriggerClientEvent('Apply:Tattoo', source, tattoo, tattooList)
+	else
+		TriggerClientEvent("QBCore:Notify", src, "You do not have enough money to purchase this tattoo!", "error")
+	end
 end)
 
 RegisterServerEvent('SmallTattoos:RemoveTattoo')
-AddEventHandler('SmallTattoos:RemoveTattoo', function(tattooList)
-    local src = source
+AddEventHandler('SmallTattoos:RemoveTattoo', function (tattooList)
+	local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    
-    exports['oxmysql']:execute('UPDATE players SET tattoos = @tattoos WHERE citizenid = @citizenid', {
-        ['@tattoos'] = json.encode(tattooList),
-        ['@citizenid'] = Player.PlayerData.citizenid
-    })
+
+	exports.oxmysql:execute('UPDATE players SET tattoos = ? WHERE citizenid = ?', {json.encode(tattooList), Player.PlayerData.citizenid})
 end)
 
 
 RegisterServerEvent('Select:Tattoos')
-AddEventHandler('Select:Tattoos', function()
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    exports['oxmysql']:execute('SELECT tattoos FROM players WHERE citizenid = @citizenid', {
-        ['@citizenid'] = Player.PlayerData.citizenid
-    }, function(result)
-        if result[1].tattoos then
-            tats = result[1].tattoos
-            TriggerClientEvent('Apply:Tattoo', src, tats)
-        else
-            TriggerEvent('remover:all')
-            TriggerClientEvent('remover:tudo', src)
-        end
-    end)
-end)
-
-RegisterServerEvent('remover:all')
-AddEventHandler('remover:all', function()
-    local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    if Player then
-        exports['oxmysql']:execute('UPDATE players SET tattoos = @tattoos WHERE citizenid = @citizenid', {
-            ['@tattoos'] = 0,
-            ['@citizenid'] = Player.PlayerData.citizenid
-        })
-    end
-end)
-
---[[RegisterServerEvent('Server:Apply:Tattoos')
-AddEventHandler('Server:Apply:Tattoos', function() 
+AddEventHandler('Select:Tattoos', function() 
 
 	local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-	exports['ghmattimysql']:execute('SELECT tattoos FROM players WHERE citizenid = @citizenid', {
-		['@citizenid'] = Player.PlayerData.citizenid
-	}, function(result)
+	exports.oxmysql:fetch('SELECT tattoos FROM players WHERE citizenid = ?', {Player.PlayerData.citizenid}, function(result)
 		if result[1].tattoos then
 			tats = result[1].tattoos
 			TriggerClientEvent('Apply:Tattoo', src, tats)
+		else
+			TriggerEvent('remover:all')
+			TriggerClientEvent('remover:tudo', src)
 		end
 	end)
+
 end)
 
-RegisterServerEvent('Receive:Tats')
-AddEventHandler('Receive:Tats', function(tats, charPed)
-	TriggerClientEvent('Apply:PedTattoo', tats, charPed)
-end)
-
-RegisterServerEvent('Select:TattoosFromPed')
-AddEventHandler('Select:TattoosFromPed', function(charPed)
+RegisterServerEvent('remover:all')
+AddEventHandler('remover:all', function() 
 	local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-	exports['ghmattimysql']:execute('SELECT tattoos FROM players WHERE citizenid = @citizenid', {
-		['@citizenid'] = Player.PlayerData.citizenid
-	}, function(result)
-		if result[1].tattoos then
-			tats = result[1].tattoos
-
-			TriggerClientEvent('Apply:PedTattoo', tats, charPed)
-		end
-	end)
-end)]]
+	local Player = QBCore.Functions.GetPlayer(src)
+	if Player then
+		exports.oxmysql:execute('UPDATE players SET tattoos = ? WHERE citizenid = ?', {0, Player.PlayerData.citizenid})
+	end
+end)
